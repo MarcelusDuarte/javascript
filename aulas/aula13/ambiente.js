f6875879b0c7f5f1bf6300acefec1d28d57a23e5
Marcelus Pereira <marcelusdp@gmail.com>
2023-05-23 22:08:02 +0100

    Estruturas de repetição

---
+++ aulas/aula13/ambiente.js
@@ -0,0 +1,26 @@
+/* repetição com teste lógico no final */
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 10)
+
+
+/* repetição com teste lógico no início */ 
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+
+ 
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
