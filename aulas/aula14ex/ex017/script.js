c6fb072d4b7026fd962519b5055801555bb13dc9
Marcelus Pereira <marcelusdp@gmail.com>
2023-05-24 20:02:05 +0100

    Exercício construindo tabuada

--- aulas/aula14ex/ex017/script.js
+++ aulas/aula14ex/ex017/script.js
@@ -1,1 +1,19 @@
-alert('Olá')
+function tabuada() {
+    //pega todos os elementos
+    var num = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        var n = Number(num.value)
+        var c = 1
+        tab.innerHTML = '' //limpa o campo select antes de iniciar novo ciclo
+        while(c <= 10) {
+            var item = document.createElement('option') // Cria o elemento option pois a tabuada se encontra em um select, que precisa do option
+            item.text = `${n} x ${c} = ${n*c}` //estrutura de cada item do campo select
+            item.value = `tab${c}` // importante para outras linguagens, como PHP. Os itens precisam ter value em outras linguagens 
+            tab.appendChild(item) //mostra os elementos. Adiciona o elemento filho
+            c++
+        }
+    }
+}
