96fa452e73b5568138df6e533400a6fdc1b47e55
Marcelus Pereira <marcelusdp@gmail.com>
2023-05-26 15:46:45 +0100

    Objetos - proximos passos

---
+++ aulas/aula17/objeto01.js
@@ -0,0 +1,16 @@
+
+/*var amigo = []
+console.log(typeof(amigo))*/
+
+var amigo = {nome:'José', 
+sexo: 'M', 
+peso: 85.4, 
+engordar(p=0){
+    console.log('Engordou')
+    this.peso += p
+
+}}
+
+amigo.engordar(2)
+console.log(`${amigo.nome} pesa ${amigo.peso} Kg`)
+
